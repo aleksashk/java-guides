11e1882de08036a3c1de13863945a89bf6f51d9f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-20 23:34:53 +0300

    @Autowired with constructor

--- src/main/java/by/flameksandr/javaguides/controller/PizzaController.java
+++ src/main/java/by/flameksandr/javaguides/controller/PizzaController.java
@@ -1,10 +1,19 @@
 package by.flameksandr.javaguides.controller;
 
+import by.flameksandr.javaguides.service.VegPizza;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("pizzaDemo")
 public class PizzaController {
+    private VegPizza vegPizza;
+
+    @Autowired
+    public PizzaController(VegPizza vegPizza) {
+        this.vegPizza = vegPizza;
+    }
+
     public String getPizza() {
-        return "Hot Pizza!";
+        return vegPizza.getPizza();
     }
 }
---
+++ src/main/java/by/flameksandr/javaguides/service/VegPizza.java
@@ -0,0 +1,10 @@
+package by.flameksandr.javaguides.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class VegPizza {
+    public String getPizza() {
+        return "Veg Pizza";
+    }
+}
