dc91ea239e7af153b3d0ea11c0719546bf0551d3
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-20 22:44:22 +0300

    Initial commit

---
+++ src/main/java/by/flameksandr/javaguides/JavaGuidesApplication.java
@@ -0,0 +1,13 @@
+package by.flameksandr.javaguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGuidesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGuidesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/flameksandr/javaguides/JavaGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.flameksandr.javaguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGuidesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
