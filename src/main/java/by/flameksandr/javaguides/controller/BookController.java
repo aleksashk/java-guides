636e02eecebef046e71deb27f7405a98b4afde3c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-21 19:12:05 +0300

    @GetMapping

--- src/main/java/by/flameksandr/javaguides/controller/BookController.java
+++ src/main/java/by/flameksandr/javaguides/controller/BookController.java
@@ -1,9 +1,8 @@
 package by.flameksandr.javaguides.controller;
 
 import by.flameksandr.javaguides.beans.Book;
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,10 +14,7 @@ public class BookController {
         return "Hello World!";
     }
 
-    @RequestMapping(value = {"/book", "/core-java", "/java"},
-            method = RequestMethod.GET,
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping("/book")
     public Book getBook() {
         return new Book(1, "Core Java", "Learn Core Java and Latest features");
     }
