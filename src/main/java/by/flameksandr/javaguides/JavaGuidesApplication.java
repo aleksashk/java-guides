f5b4fa88dfa1d1339011c9b7762f809fe560c702
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-21 13:36:03 +0300

    @Scope refactoring

--- src/main/java/by/flameksandr/javaguides/JavaGuidesApplication.java
+++ src/main/java/by/flameksandr/javaguides/JavaGuidesApplication.java
@@ -11,12 +11,22 @@ public class JavaGuidesApplication {
     public static void main(String[] args) {
         var context = SpringApplication.run(JavaGuidesApplication.class, args);
         SingletonBean singletonBean1 = context.getBean(SingletonBean.class);
+        System.out.println("singletonBean1.hashCode() " + singletonBean1.hashCode());
+
         SingletonBean singletonBean2 = context.getBean(SingletonBean.class);
+        System.out.println("singletonBean2.hashCode() " + singletonBean2.hashCode());
+
         SingletonBean singletonBean3 = context.getBean(SingletonBean.class);
+        System.out.println("singletonBean3.hashCode() " + singletonBean3.hashCode());
 
         PrototypeBean prototypeBean1 = context.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1.hashCode() " + prototypeBean1.hashCode());
+
         PrototypeBean prototypeBean2 = context.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean2.hashCode() " + prototypeBean2.hashCode());
+
         PrototypeBean prototypeBean3 = context.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean3.hashCode() " + prototypeBean3.hashCode());
 
 
     }
