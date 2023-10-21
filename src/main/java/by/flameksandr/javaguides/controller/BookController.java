9391827dc7721efe6dc19c51a69806a593b87497
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-21 15:30:58 +0300

    @RequestMapping with Http Method

--- src/main/java/by/flameksandr/javaguides/controller/BookController.java
+++ src/main/java/by/flameksandr/javaguides/controller/BookController.java
@@ -2,6 +2,7 @@ package by.flameksandr.javaguides.controller;
 
 import by.flameksandr.javaguides.beans.Book;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +14,8 @@ public class BookController {
         return "Hello World!";
     }
 
-    @RequestMapping(value = {"/book", "/core-java", "/java"})
+    @RequestMapping(value = {"/book", "/core-java", "/java"},
+            method = RequestMethod.GET)
     public Book getBook() {
         return new Book(1, "Core Java", "Learn Core Java and Latest features");
     }
