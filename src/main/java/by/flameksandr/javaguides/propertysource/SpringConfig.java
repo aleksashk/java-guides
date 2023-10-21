2f9e3c2dec8649182a4b923229e2a324422b0c2c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-21 14:40:38 +0300

    @PropertySources

--- src/main/java/by/flameksandr/javaguides/propertysource/SpringConfig.java
+++ src/main/java/by/flameksandr/javaguides/propertysource/SpringConfig.java
@@ -2,10 +2,15 @@ package by.flameksandr.javaguides.propertysource;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySource("classpath:mail.properties")
-@PropertySource("classpath:messages.properties")
+//@PropertySource("classpath:mail.properties")
+//@PropertySource("classpath:messages.properties")
+@PropertySources({
+        @PropertySource("classpath:mail.properties"),
+        @PropertySource("classpath:messages.properties")
+})
 public class SpringConfig {
 
 }
