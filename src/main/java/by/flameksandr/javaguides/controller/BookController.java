22f924cb393cc8577debb38f7ba562e0463a1260
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-21 15:16:11 +0300

    @RestController=@Controller+@ResponseBody

--- src/main/java/by/flameksandr/javaguides/controller/BookController.java
+++ src/main/java/by/flameksandr/javaguides/controller/BookController.java
@@ -1,21 +1,18 @@
 package by.flameksandr.javaguides.controller;
 
 import by.flameksandr.javaguides.beans.Book;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BookController {
 
     @RequestMapping("/hello-world")
-    @ResponseBody
     public String helloWorld() {
         return "Hello World!";
     }
 
     @RequestMapping("/book")
-    @ResponseBody
     public Book getBook() {
         Book book = new Book(1, "Core Java", "Learn Core Java and Latest features");
         return book;
