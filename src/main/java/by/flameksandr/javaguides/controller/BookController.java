ee865113be3e69ece71f8bf57f3fe8bd5079d632
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-21 19:18:41 +0300

    method = RequestMethod.POST

--- src/main/java/by/flameksandr/javaguides/controller/BookController.java
+++ src/main/java/by/flameksandr/javaguides/controller/BookController.java
@@ -3,6 +3,7 @@ package by.flameksandr.javaguides.controller;
 import by.flameksandr.javaguides.beans.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,14 @@ public class BookController {
     @GetMapping(value = {"/book", "/java-core", "/java"})
     public Book getBook() {
         return new Book(1, "Core Java", "Learn Core Java and Latest features");
+    }
+
+    @RequestMapping(value = "/books/create", method = RequestMethod.POST)
+    public Book createBook(Book book){
+        System.out.println(book.getId());
+        System.out.println(book.getTitle());
+        System.out.println(book.getDescription());
+
+        return book;
     }
 }
