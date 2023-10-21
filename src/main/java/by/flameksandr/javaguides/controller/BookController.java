8bef482ac36e688dc603dbb54d026df8a885fe18
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-21 15:34:35 +0300

    @RequestMapping with Produces and Consumers

--- src/main/java/by/flameksandr/javaguides/controller/BookController.java
+++ src/main/java/by/flameksandr/javaguides/controller/BookController.java
@@ -1,6 +1,7 @@
 package by.flameksandr.javaguides.controller;
 
 import by.flameksandr.javaguides.beans.Book;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,9 @@ public class BookController {
     }
 
     @RequestMapping(value = {"/book", "/core-java", "/java"},
-            method = RequestMethod.GET)
+            method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public Book getBook() {
         return new Book(1, "Core Java", "Learn Core Java and Latest features");
     }
